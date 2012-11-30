2c508700c555389a750b385633bf0408c4ee54a3
abimaran <k.abimaran@gmail.com>
2012-11-30 20:52:40 +0530

    Added about button, color to UIs

---
+++ src/com/example/About.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ABI
+ * Date: 11/29/12
+ * Time: 11:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+
+}
